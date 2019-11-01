8740bcc1ba7eb2b416813216c479be8bbefa70aa
Diego Abrantes Alves <diego.2006@hotmail.com>
2019-11-01 19:47:13 -0300

    Criação do projeto de teste

---
+++ Projeto.DDD.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Projeto.DDD.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
